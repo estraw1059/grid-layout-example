e2fb4f3dd5b48b5ae93881b4dd5e99ec78247d03
Eric <estraw@smu.edu>
2025-02-15 13:23:20 -0600

    Saving and loading of bar is possible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,63 +3,87 @@ import GridLayout, { ItemCallback, Layout } from "react-grid-layout";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import { JSX, useEffect, useState } from "react";
-import layoutData from "./layout.json";
-
 
 export default function Home() {
-  const layout: Layout[] = layoutData;
-
+  const [loading, setLoading] = useState(true);
+  const [layout, setLayout] = useState<Layout[]>([]);
   const [boxes, setBoxes] = useState<JSX.Element[]>([]);
 
   useEffect(() => {
-    const newBoxes = layout.map(item => (
-      <div key={item.i} className="border border-black">{item.i}</div>
-    ));
-    setBoxes(newBoxes);
-  }, [layout]);
-
+    // Fetch the latest layout data from the server
+    fetch('/api/grid')
+      .then(response => response.json())
+      .then(data => {
+        const gridItems = data.map((item: { i: string }) => (
+          <div key={item.i} className="border border-black">{item.i}</div>
+        ));
+        setBoxes(gridItems);
+        setLayout(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching layout:', error);
+      });
+  }, []);
 
   const addNewBox = () => {
-    layout.push({
+    const newItem = {
       i: "n" + new Date().getTime(),
       x: (layout.length * 2) % (12 - 2),
       y: Infinity, // puts it at the bottom
       w: 2,
       h: 2
-    });
-    const newBoxes = layout.map(item => (
+    };
+    const newLayout = [...layout, newItem];
+    const newBoxes = newLayout.map(item => (
       <div key={item.i} className="border border-black">{item.i}</div>
     ));
     setBoxes(newBoxes);
+    setLayout(newLayout);
   };
 
+  const saveLayout = () => {
+    console.log("Saving layout:", layout);
+    fetch('/api/grid', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(layout)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
   const handleItemCallback: ItemCallback = (
-    layout: Layout[],
-    oldItem: Layout,
-    newItem: Layout,
-    placeholder: Layout,
-    event: MouseEvent,
-    element: HTMLElement
+    newLayout: Layout[],
   ) => {
-    console.log("Layout:", layout);
-    console.log("Old Item:", oldItem);
-    console.log("New Item:", newItem);
-    console.log("Placeholder:", placeholder);
-    console.log("Event:", event);
-    console.log("Element:", element);
+    setLayout(newLayout);
   };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
+    <div className="h-screen w-screen">
+      <button onClick={saveLayout}>Save Layout</button>
+      <br/>
       <button onClick={addNewBox}>Add new box</button>
       <GridLayout
-        className="layout border-black"
+        className="layouts border-black"
         onResizeStop={handleItemCallback}
         onDragStop={handleItemCallback}
         layout={layout}
         cols={12}
         rowHeight={30}
         width={1200}
+        autoSize={true}
       >
         {boxes}
       </GridLayout>
